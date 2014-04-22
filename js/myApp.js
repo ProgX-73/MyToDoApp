95872e9679f7aaec335063b353e06fd40ef433b7
Fred <progx73@gmail.com>
2014-04-22 14:58:06 +0200

    Gestion des evenement

--- js/myApp.js
+++ js/myApp.js
@@ -1,45 +1,92 @@
-app = angular.module('myApp',[]);
+app = angular.module('myApp', []);
 
-app.controller('listController',function($scope){
+app.controller('eventController', function ($rootScope) {
     
+     $rootScope.$on('updateDetail',function(event, data){
+        
+         alert("by ROOT");
+         //relai de com!
+        $rootScope.$broadcast('updateDetail',event, data);
+        
+    });
+});
+    
+app.controller('listController', function ($scope, TodoService) {
+
+   
+   
     $scope.titre = "Hello World !!!";
-    
-    $scope.todos = [ 
-        { "id" : 0, "titre":"Matin", "tache":"se lever", date_enr :"2014-04-21", date_max:"2014-04-23"},
-        { "id" : 1, "titre":"AM", "tache":"courir", date_enr :"2014-04-25", date_max:"2014-04-25"},
-        { "id" : 2, "titre":"PM", "tache":"ramer", date_enr :"2014-04-23", date_max:"2014-04-29"}
-           
-    ]
-    
-});
 
+    $scope.go = function (m) {
 
-app.controller('navController',function($scope){
-    
-    $scope.hash ="2a0983ec57054ee1d0277dc8bda17cb9.png?s=28";
-});
-
-app.controller('detailController',function($scope,TodoService){
-    
-    var luke = new TodoService('Luke');
-    $scope.detail  = luke.speak();
-
-});
-
-
-app.factory('TodoService', function() {
-  var TodoClass = function(user) {
-    this.name = user;
-    this.speak = function () {
-      return 'liste des todos  ' + this.name;
+       var todo = TodoService.find(m);
+       $scope.$emit('updateDetail',todo);
+        
     };
-    this.datas =  [ 
-        { "id" : 0, "titre":"Matin", "tache":"se lever", date_enr :"2014-04-21", date_max:"2014-04-23"},
-        { "id" : 1, "titre":"AM", "tache":"courir", date_enr :"2014-04-25", date_max:"2014-04-25"},
-        { "id" : 2, "titre":"PM", "tache":"ramer", date_enr :"2014-04-23", date_max:"2014-04-29"}
-           
-    ]
-  }
-  return TodoClass;
+    
+    
+    $scope.todos = TodoService.findAll();
+
+   
 });
- 
+
+
+app.controller('navController', function ($scope) {
+
+    $scope.hash = "2a0983ec57054ee1d0277dc8bda17cb9.png?s=28";
+});
+
+app.controller('detailController', function ($scope, TodoService) {
+
+    $scope.sel = "Loading ...";
+    $scope.$on('updateDetail',function(event, data){
+        
+        $scope.sel = data;
+        alert("updated");
+    });
+    
+    
+});
+
+
+app.factory('TodoService', function () {
+
+   var manager = {
+    selection : {},
+    find : function(id){
+        
+        this.selection =  this.todos[id];
+        return this.todos[id];},
+    findAll : function(){return this.todos;},
+    todos : [
+            {
+                "id": 0,
+                "titre": "Matin",
+                "tache": "se lever",
+                "user": "fred",
+                date_enr: "2014-04-21",
+                date_max: "2014-04-23"
+        },
+            {
+                "id": 1,
+                "titre": "AM",
+                "tache": "courir",
+                "user": "fred",
+                date_enr: "2014-04-25",
+                date_max: "2014-04-25"
+        },
+            {
+                "id": 2,
+                "titre": "PM",
+                "tache": "ramer",
+                "user": "oliv",
+                date_enr: "2014-04-23",
+                date_max: "2014-04-29"
+        }
+
+    ]}
+            return manager;
+    
+    
+    });
+    
