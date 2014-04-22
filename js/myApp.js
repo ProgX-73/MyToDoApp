c739bc632fab1e9371681064e57bdeeffdec84fa
Fred <progx73@gmail.com>
2014-04-22 12:19:43 +0200

    Interface
    Réalisée et logique ang

--- js/myApp.js
+++ js/myApp.js
@@ -12,3 +12,34 @@ app.controller('listController',function($scope){
     ]
     
 });
+
+
+app.controller('navController',function($scope){
+    
+    $scope.hash ="2a0983ec57054ee1d0277dc8bda17cb9.png?s=28";
+});
+
+app.controller('detailController',function($scope,TodoService){
+    
+    var luke = new TodoService('Luke');
+    $scope.detail  = luke.speak();
+
+});
+
+
+app.factory('TodoService', function() {
+  var TodoClass = function(user) {
+    this.name = user;
+    this.speak = function () {
+      return 'liste des todos  ' + this.name;
+    };
+    this.datas =  [ 
+        { "id" : 0, "titre":"Matin", "tache":"se lever", date_enr :"2014-04-21", date_max:"2014-04-23"},
+        { "id" : 1, "titre":"AM", "tache":"courir", date_enr :"2014-04-25", date_max:"2014-04-25"},
+        { "id" : 2, "titre":"PM", "tache":"ramer", date_enr :"2014-04-23", date_max:"2014-04-29"}
+           
+    ]
+  }
+  return TodoClass;
+});
+ 
